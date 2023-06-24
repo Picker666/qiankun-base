c22598febe36e9af19980d7579e7de08022cf91b
Christine-Only <tsy17321289030@163.com>
2023-06-24 18:48:35 +0800

    some change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import { useState, useMemo, useRef } from 'react';
+import { Link, useLocation } from 'react-router-dom'
+import { Layout, Menu } from 'antd';
+import { DesktopOutlined, PieChartOutlined } from '@ant-design/icons';
+
 import './App.css';
 
-function App() {
+const { Content, Footer, Sider } = Layout;
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const location = useLocation();
+
+  const defaultKey = useRef(location.pathname.split('/')[1]);
+  const onCollapse = collapsed => {
+    setCollapsed(collapsed);
+  };
+
+  const items = useMemo(() => [{
+    label: <Link to="/app-vue">Vue应用</Link>,
+    icon: <DesktopOutlined/>,
+    key: 'app-vue',
+    title: 'Vue应用',
+  },{
+    label: <Link to="/app-react">React应用</Link>,
+    icon: <PieChartOutlined />,
+    key: 'app-react',
+    title: 'React应用'
+  }], []);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <Menu theme="dark" defaultSelectedKeys={[defaultKey.current]} mode="inline" items={items} />
+      </Sider>
+      <Layout className="site-layout">
+        <Content>
+          <div id="container" className="site-layout-background" style={{ minHeight: 360 }}></div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>This Project ©2021 Created by DiDi</Footer>
+      </Layout>
+    </Layout>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,101 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { registerMicroApps, start, setDefaultMountApp, runAfterFirstMounted, initGlobalState } from 'qiankun';
+
+import './index.css';
+
+const apps = [
+  {
+    name: 'vueApp',
+    entry: '//localhost:8081',
+    container: '#container',
+    activeRule: '/app-vue',
+    loader: (loading) => {
+      console.log('vueApp, loading: ', loading);
+    },
+    props: {
+      name: 'vueApp',
+    }
+  },
+  {
+    name: 'reactApp',
+    entry: '//localhost:3002',
+    container: '#container',
+    activeRule: (location) => {
+      return location.pathname.startsWith('/app-react');
+    },
+    loader: (loading) => {
+      console.log('reactApp, loading: ', loading);
+    },
+    props: {
+      name: 'reactApp'
+    }
+  },
+];
+
+const lifeCycles = {
+  beforeLoad: (app) => {
+    console.log('app beforeLoad: ', app);
+  },
+  beforeMount: [(app) => {
+    console.log('app beforeMount: ', app);
+  }],
+  afterMount: [
+    (app) => {
+      console.log('app afterMount1: ', app);
+    },
+    (app) => {
+      console.log('app afterMount2: ', app);
+    }
+  ],
+  beforeUnmount: (app) => {
+    console.log('app beforeUnmount: ', app);
+  },
+  afterUnmount: (app) =>{
+    console.log('app beforeUnmount: ', app);
+  }
+}
+
+registerMicroApps(
+  apps,
+  lifeCycles,
+);
+
+start({
+  prefetch: function(a) {
+    console.log('a======: ', a);
+    return {
+      criticalAppNames: ['reactApp']
+    }
+  },
+  sandbox: {
+    experimentalStyleIsolation: true,
+    // strictStyleIsolation:  true,
+  }
+});
+
+setDefaultMountApp('/app-react');
+
+runAfterFirstMounted(() => {
+  console.log('=====runAfterFirstMounted======');
+});
+
+// 初始化 state
+const actions = initGlobalState({rootCount: 0});
+
+actions.onGlobalStateChange((state, prev) => {
+  console.log('state,=============== prev: ', state, prev);
+  // state: 变更后的状态; prev 变更前的状态
+});
+actions.setGlobalState({count: 2});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
